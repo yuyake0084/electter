b682b181ec58e9b4feb189d68520cd4b771b76e6
ohashiyu <ohashiyu@oohashiisamu-no-MacBook-Pro.local>
2017-01-24 18:31:30 +0900

    initial commit

---
+++ src/app/javascripts/main/timeline-window.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron');
+
+module.exports = class TimlineWindow {
+  constructor() {
+    this.window = null;
+    this.start();
+  }
+
+  start() {
+    app.on('ready', () => this.createWindow());
+  }
+
+  createWindow() {
+    this.window = new BrowserWindow({
+      x: 0,
+      y: 0,
+      width: 400,
+      height: 800
+    });
+
+    this.window.loadURL(`file://${__dirname}../../../html/main.html`);
+  }
+}
